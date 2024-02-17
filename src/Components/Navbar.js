ef99fbf3a6e731c861e501b0e4d123afc91568bb
Mahek shivani <117032523+mahekshivaniii@users.noreply.github.com>
2024-02-17 13:17:46 +0530

    abc

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,7 +11,7 @@ export class Navbar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">AestheticNewspapers</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -34,14 +34,9 @@ export class Navbar extends Component {
             <li><a className="dropdown-item" href="">Something else here</a></li>
           </ul>
         </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
+        
       </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+     
     </div>
   </div>
 </nav>
