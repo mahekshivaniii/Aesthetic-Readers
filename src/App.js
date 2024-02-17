81a4602ed39c67da375723532c8af7fb14bad76b
Mahek shivani <117032523+mahekshivaniii@users.noreply.github.com>
2024-02-17 13:13:20 +0530

    adding components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
 
 export default class App extends Component {
-  c=5
+  
   render() {
     return (
       <div>
-        hello everyone i am react {this.c}
+    
+       <Navbar></Navbar>
+     
       </div>
     )
   }
