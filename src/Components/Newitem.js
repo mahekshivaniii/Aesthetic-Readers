e5f05911262f8b1d30702ce5dd37f6d0540f0cb9
Mahek shivani <117032523+mahekshivaniii@users.noreply.github.com>
2024-02-20 12:52:29 +0530

    commit

--- src/Components/Newitem.js
+++ src/Components/Newitem.js
@@ -4,13 +4,13 @@ export default class Newitem extends Component {
   render() {
     let {title,description,imageUrl} = this.props
     return (
-      <div>
+      <div className='my-3'>
         <div className="card" style ={{ width:"18rem"}}>
   <img src={imageUrl} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}</p>
-    <a href="/newDetail" className="btn btn-primary">Go somewhere</a>
+    <a href="/newDetail" className="btn btn-sm btn-primary">Read More</a>
   </div>
 </div>
       </div>
--- src/Components/News.js
+++ src/Components/News.js
@@ -2,6 +2,56 @@ import React, { Component } from 'react'
 import Newitem from './Newitem'
 
 export default class News extends Component {
+  articles =  [
+    {
+        "source": {
+            "id": "news24",
+            "name": "News24"
+        },
+        "author": null,
+        "title": "News24 | Mike Procter, SA cricket legend and Proteas' first post-isolation coach, dies at 77",
+        "description": "World cricketing great Mike Procter died in an Umhlanga private hospital on Saturday afternoon.",
+        "url": "https://www.news24.com/sport/cricket/proteas/mike-procter-sa-cricket-legend-and-proteas-first-post-isolation-coach-dies-at-77-20240217",
+        "urlToImage": null,
+        "publishedAt": "2024-02-17T19:37:01",
+        "content": "World cricketing great Mike Procter died in an Umhlanga private hospital on Saturday afternoon."
+    },
+    {
+        "source": {
+            "id": "espn-cric-info",
+            "name": "ESPN Cric Info"
+        },
+        "author": null,
+        "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+        "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+        "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+        "publishedAt": "2020-04-27T11:41:47Z",
+        "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+    },
+    {
+        "source": {
+            "id": "espn-cric-info",
+            "name": "ESPN Cric Info"
+        },
+        "author": null,
+        "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+        "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+        "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+        "publishedAt": "2020-03-30T15:26:05Z",
+        "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+    }
+]
+  constructor(){
+    super()
+    console.log("clear")
+    this.state={
+          articles :this.articles,
+          loading : false
+
+    }
+  }
   render() {
    
     return (
@@ -9,13 +59,13 @@ export default class News extends Component {
         <h2>Aesthetic Newspapers</h2>
         <div className="row">
           <div className="col-md-4">
-        <Newitem title="first title" description="first desc" imageUrl="https://www.news24.com/sport/cricket/proteas/mike-procter-sa-cricket-legend-and-proteas-first-post-isolation-coach-dies-at-77-20240217"></Newitem>
+        <Newitem title="first title" description="first desc" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg"></Newitem>
         </div>
         <div className="col-md-4">
-        <Newitem title="first title" description="first desc"></Newitem>
+        <Newitem title="first title" description="first desc" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg"></Newitem>
         </div>
         <div className="col-md-4">
-        <Newitem title="first title" description="first desc"></Newitem>
+        <Newitem title="first title" description="first desc" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg"></Newitem>
         </div>
            </div>
       </div>
